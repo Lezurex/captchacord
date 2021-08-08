06efb3126f0ce6e459c1b58af2025e8fd6cdb62e
Lenny Angst <lenny.angst@easyid.ch>
2021-08-08 13:54:45 +0200

    Initial commit

---
+++ src/main/java/com/lezurex/captchacord/App.java
@@ -0,0 +1,20 @@
+package com.lezurex.captchacord;
+
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+
+public class App {
+
+
+
+    public static void main(String[] args) {
+        new App();
+    }
+
+    public App() {
+    }
+
+    private void createBot() {
+
+    }
+}
