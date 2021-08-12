e2ac8e7704908d9933a64e726ec1d710f1e04037
Lenny Angst <lenny.angst@easyid.ch>
2021-08-12 21:12:31 +0200

    Add cli log level parameter

--- src/main/java/com/lezurex/captchacord/App.java
+++ src/main/java/com/lezurex/captchacord/App.java
@@ -19,6 +19,9 @@ import lombok.extern.log4j.Log4j2;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
+import org.apache.commons.cli.*;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.core.config.Configurator;
 
 import javax.security.auth.login.LoginException;
 
@@ -27,17 +30,45 @@ import javax.security.auth.login.LoginException;
 public class App {
 
     public static void main(String[] args) {
-        // Method empty because the instance is instantiated in assigning of instance static variable
+        CommandLine commandLine;
+        Option logOption = Option.builder("L").required(false).hasArg().desc("Sets the logging level.").longOpt("loglevel").build();
+        Options options = new Options();
+        options.addOption(logOption);
+        CommandLineParser parser = new DefaultParser();
+
+        try {
+            commandLine = parser.parse(options, args);
+            if (commandLine.hasOption("L")) {
+                String value = commandLine.getOptionValue("L");
+                Level level = Level.getLevel(value);
+                if (level != null)
+                    Configurator.setRootLevel(level);
+                else {
+                    log.printf(Level.ERROR, "'%s' is not a valid log level!", value);
+                    System.exit(1);
+                }
+            }
+        } catch (ParseException e) {
+            log.error("Invalid arguments!");
+            System.exit(1);
+        }
+
+        log.debug("DEBUG");
+        log.info("INFO");
+        log.warn("WARN");
+
+        new App();
     }
 
     @Getter
-    private static final App instance = new App();
+    private static App instance;
 
     private final BotConfig botConfig;
     private JDA jda;
     private final CommandManager commandManager;
 
     public App() {
+        instance = this;
         log.info("Hello darling my old frend");
         ConfigLoader configLoader = new ConfigLoader();
         this.botConfig = configLoader.load();
